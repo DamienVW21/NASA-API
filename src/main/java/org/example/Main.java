1e3851a727f6c0eb8879c728dd8b3b759dacc98d
Damien <dvw2011@hotmail.com>
2025-10-16 20:23:55 -0400

    Endpoints Connected and added red text functionality for hazardous NEOs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import org.example.model.Neo;
+import org.example.model.NeoFeedResponse;
+import org.example.service.NASAService;
+
 import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +17,41 @@ public class Main {
         String sDate = userInput.nextLine();
 
         // might have to turn into local date object, to pass to API?
+        NASAService service = new NASAService();
+        NeoFeedResponse response = service.getAllNeos(sDate, sDate);
+
+        // for each loop to loop through the keys(dates)
+
+        for ( String key: response.getNearEarthObjects().keySet()){
+            // grab list (values) based on the key (date)
+            List<Neo> neoList = response.getNearEarthObjects().get(key);
+            int count = neoList.size();
+            System.out.println("For date: " + key + " there are " + count + " near earth objects");
+
+            String code = "\\x1b[0m";
+            for (Neo n: neoList){
+                if(n.isIspotentiallyHazardousAsteroid()){
+                    code = "\u001B[31m";
+                }
+                else{
+                    code = "\u001B[0m";
+                }
+                System.out.println(code + "Id: " + n.getId());
+                System.out.println("name: " + n.getName());
+                System.out.println("Potentially Hazardous? " + n.isIspotentiallyHazardousAsteroid());
+                System.out.println("Estimated Diameter : ");
+                System.out.println("\tMin in Miles: " +
+                        n.getEstimatedDiameter().getMiles().getEstimatedDiameterMin());
+                System.out.println("\tMax in Miles: " +
+                        n.getEstimatedDiameter().getMiles().getEstimatedDiameterMax());
+                System.out.println("\tMin in Feet: " +
+                        n.getEstimatedDiameter().getFeet().getEstimatedDiameterMin());
+                System.out.println("\tMax in Feet: " +
+                        n.getEstimatedDiameter().getFeet().getEstimatedDiameterMax());
+                System.out.println("\n***********************");
+
+            }
+        }
 
     }
 }
--- src/main/java/org/example/service/NASAService.java
+++ src/main/java/org/example/service/NASAService.java
@@ -1,5 +1,6 @@
 package org.example.service;
 
+import org.example.model.NeoFeedResponse;
 import org.springframework.web.client.RestTemplate;
 
 public class NASAService {
@@ -9,5 +10,20 @@ public class NASAService {
     private final String API_URL = "https://api.nasa.gov/neo/rest/v1/feed?";
 
     private final String API_KEY = System.getenv("NASA_KEY");
+
+
+    // Request to get all NEOs for a specified group of days
+    public NeoFeedResponse getAllNeos(String startDate, String endDate){
+        String url = API_URL + "start_date=" + startDate +
+                "&end_date=" + endDate +
+                "&api_key=" + API_KEY;
+
+       // String response = restTemplate.getForObject(url, String.class); //.toString();
+       // System.out.println(response);
+
+        NeoFeedResponse response = restTemplate.getForObject(url, NeoFeedResponse.class);
+
+        return response;
+    }
 
 }
